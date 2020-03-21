b8caf5e1a83ae2da5e16a8bfd557146f53bf5325
Shawn Baker <shawn@frozen.ca>
2020-03-21 13:31:22 -0500

    Move the actual authentication code into LoginPage.

--- Example/Example/MainPage.xaml.cs
+++ Example/Example/MainPage.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel;
-using System.Timers;
+﻿// Copyright © 2020 Shawn Baker using the MIT License.
+using System;
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Essentials;
 using FrozenNorth.SpotifyAuth;
@@ -9,50 +10,21 @@ namespace Example
 	[DesignTimeVisible(false)]
 	public partial class MainPage : ContentPage
 	{
-		// authentication constants
-		private const string AuthClientId = "clientid";
-		private const string AuthUrl = "authurl";
-		private const string AuthRedirectUrl = "redirecturl";
-		private const Scope AuthScope = //Scope.AppRemoteControl | 
-										Scope.PlaylistModifyPrivate |
-										Scope.PlaylistModifyPublic |
-										Scope.PlaylistReadCollaborative |
-										Scope.PlaylistReadPrivate |
-										Scope.Streaming |
-										// Scope.UgcImageUpload |
-										Scope.UserFollowModify |
-										Scope.UserFollowRead |
-										Scope.UserLibraryModify |
-										Scope.UserLibraryRead |
-										Scope.UserModifyPlaybackState |
-										Scope.UserReadCurrentlyPlaying |
-										Scope.UserReadEmail |
-										Scope.UserReadPlaybackState |
-										Scope.UserReadPrivate |
-										Scope.UserReadRecentlyPlayed |
-										Scope.UserTopRead;
-
-		// instance variables
-		private Timer timeoutTimer = null;
-
 		/// <summary>
-		/// Initializes the page and starts authentication.
+		/// Constructor - Initializes the page and starts authentication.
 		/// </summary>
 		public MainPage()
 		{
 			InitializeComponent();
 
 			Authenticator.AuthChanged += Authenticator_AuthChanged;
-			Authenticator.GetCodeAsync(AuthClientId, AuthScope, AuthUrl, AuthRedirectUrl);
-			StartTimeoutTimer();
-			SetState("");
+			loginButton_Clicked(this, EventArgs.Empty);
 		}
 
 		/// <summary>
 		/// Configures the UI based on the current state.
 		/// </summary>
-		/// <param name="error">Optional error message</param>
-		private void SetState(string error = null)
+		private void SetState()
 		{
 			MainThread.BeginInvokeOnMainThread(() =>
 			{
@@ -74,29 +46,24 @@ namespace Example
 					loginButton.Text = "Logout";
 					refreshButton.IsEnabled = true;
 				}
-				if (error != null)
-				{
-					errorLabel.Text = error;
-				}
 			});
 		}
 
 		/// <summary>
 		/// Starts or stops the authentication process.
 		/// </summary>
-		private void loginButton_Clicked(object sender, System.EventArgs e)
+		private async void loginButton_Clicked(object sender, System.EventArgs e)
 		{
 			if (loginButton.Text == "Login")
 			{
-				Authenticator.GetCodeAsync(AuthClientId, AuthScope, AuthUrl, AuthRedirectUrl);
-				StartTimeoutTimer();
+				LoginPage loginPage = new LoginPage();
+				await Navigation.PushAsync(loginPage);
 			}
 			else
 			{
 				Authenticator.Reset();
-				StopTimeoutTimer();
 			}
-			SetState("");
+			SetState();
 		}
 
 		/// <summary>
@@ -105,49 +72,12 @@ namespace Example
 		private void refreshButton_Clicked(object sender, System.EventArgs e)
 		{
 			Authenticator.Refresh();
-			StopTimeoutTimer();
 			SetState();
 		}
 
 		private void Authenticator_AuthChanged(object sender, System.EventArgs e)
 		{
-			StopTimeoutTimer();
-			SetState("");
-		}
-
-		/// <summary>
-		/// Start the timeout timer.
-		/// </summary>
-		private void StartTimeoutTimer()
-		{
-			StopTimeoutTimer();
-			timeoutTimer = new Timer(10000);
-			timeoutTimer.Elapsed += HandleTimeoutTimerElapsed;
-			timeoutTimer.AutoReset = false;
-			timeoutTimer.Start();
-		}
-
-		/// <summary>
-		/// Stop the timeout timer.
-		/// </summary>
-		private void StopTimeoutTimer()
-		{
-			if (timeoutTimer != null)
-			{
-				timeoutTimer.Stop();
-				timeoutTimer.Dispose();
-				timeoutTimer = null;
-			}
-		}
-
-		/// <summary>
-		/// Refresh the access token when the timer goes off.
-		/// </summary>
-		private void HandleTimeoutTimerElapsed(object sender, ElapsedEventArgs e)
-		{
-			StopTimeoutTimer();
-			Authenticator.Reset();
-			SetState("Request timed out.");
+			SetState();
 		}
 	}
 }
