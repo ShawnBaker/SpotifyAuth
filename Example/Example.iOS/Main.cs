a06447288d78d44e00c23d28c4cf2438f9158260
Shawn Baker <shawn@frozen.ca>
2020-03-22 21:02:37 -0500

    Code cleanup.

--- Example/Example.iOS/Main.cs
+++ Example/Example.iOS/Main.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿// Copyright © 2020 Shawn Baker using the MIT License.
 using UIKit;
 
 namespace Example.iOS
 {
     public class Application
     {
-        // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
