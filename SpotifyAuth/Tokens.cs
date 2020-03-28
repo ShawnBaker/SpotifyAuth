397e9bfabddf8d05f456a12d2b9404d24131dd11
Shawn Baker <shawn@frozen.ca>
2020-03-28 11:12:51 -0500

    This class is now internal.

--- SpotifyAuth/Tokens.cs
+++ SpotifyAuth/Tokens.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace FrozenNorth.SpotifyAuth
 {
-    public class Tokens
+    internal class Tokens
     {
         [JsonProperty("access_token")]
         public string AccessToken { get; set; }
